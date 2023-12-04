4b949a47cf7bc7755c2492fe80cd6c2aaec63a9b
whereisjw <j9399977@gmail.com>
2023-12-04 12:00:00 +0900

    쌩 노드로 path나누기

---
+++ test/server.js
@@ -0,0 +1,23 @@
+const http = require("http");
+const url = require("url");
+
+const host = "localhost"; // loop back (서버를 실행한 컴퓨터 127.0.0.1)
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  const path = url.parse(req.url).pathname;
+
+  if (path === "/") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>home page</h1>");
+  } else if (path === "/post") {
+    res.writeHead(200, { "Content-Type": "text/html" });
+    res.end("<h1>post page</h1>");
+  }
+
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>hello world</h1>");
+});
+server.listen(port, host, () => {
+  console.log("http://localhost:3000 실행중");
+});
